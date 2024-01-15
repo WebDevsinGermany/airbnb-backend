74ee0cf16c44befbd0318b0a64a78657d5e4de43
Seung-Jae Yoon <casysj@hotmail.com>
2024-01-15 15:57:31 +0100

    init commit (under construction)

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+const dbConfig = {
+    type: 'postgresql',
+    database: 'qzqgbckb',
+    host: 'tyke.db.elephantsql.com',
+    username: 'qzqgbckb',
+    password: 'MW5KJ4IPetGJ8aJ-SXRNVrIzC1NoO1D7',
+    entities: ['**/*.entity.ts'],
+    synchronize: false
+}
+
+
+module.exports = dbConfig;
